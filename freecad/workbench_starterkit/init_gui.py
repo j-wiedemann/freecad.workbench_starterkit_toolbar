20ca38fe5b7b0a00ba5364fc98719045765d2f96
Wiedemann Jonathan <contact@freecad-france.com>
2022-09-05 10:29:24 +0200

    Add group parameter and a first startup message

--- freecad/workbench_starterkit/init_gui.py
+++ freecad/workbench_starterkit/init_gui.py
@@ -41,6 +41,48 @@ class TemplateWorkbench(Gui.Workbench):
         '''
         code which should be computed when a user switch to this workbench
         '''
+        # This command will get group of parameters if it exist otherwise it will create a new one 
+        p = App.ParamGet("User parameter:BaseApp/Preferences/Mod/workbench_starterkit")
+        
+        
+        '''
+        Theses commands will check if this is the first time the user activated this workbench,
+        it will use parameter to check the first_startup status and store the new status
+        '''
+        is_first_startup = True
+        # First check if there is article in group parameters
+        if not p.IsEmpty():
+            for article in p.GetContents():
+                if "first_startup" in article:
+                    # first_startup parameter exist so let's get is value
+                    is_first_startup = p.GetBool("first_startup")
+                else:
+                    # If no article then create a boolean one called "first_startup" with True value
+                    p.SetBool("first_startup", is_first_startup)
+        else:
+            # If no article then create a boolean one called "first_startup" with True value
+            p.SetBool("first_startup", is_first_startup)
+        # now show a QMessageBox if this is the first startup
+        if is_first_startup is True:
+            App.Console.PrintMessage("Welcome, this is your first time with this workbench\n")
+            from PySide import QtGui
+            mb = QtGui.QMessageBox()
+            mb.setWindowTitle("First time?")
+            mb.setText("Do you want to see a tutorial?")
+            cb = QtGui.QCheckBox("Don't show this message again")
+            mb.setCheckBox(cb)
+            mb.setStandardButtons(mb.Yes | mb.No)
+            reply = mb.exec_()
+            if cb.isChecked():
+                p.SetBool("first_startup", False)
+            else:
+                p.SetBool("first_startup", True)
+            if reply == mb.No:
+                App.Console.PrintMessage("Ok, no problem\n")
+                pass
+            elif reply == mb.Yes:
+                App.Console.PrintMessage("Ok, let's go!\n")
+                pass
         pass
 
     def Deactivated(self):
